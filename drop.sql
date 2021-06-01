ef5d23b2f8da98b69e9f5e0b3b3b4a0d050d4a67
Warpand <maciek@Mac-Laptop.localdomain>
2021-06-01 18:58:20 +0200

    in i drop

---
+++ drop.sql
@@ -0,0 +1,74 @@
+begin;
+
+DROP VIEW if exists payments;
+DROP VIEW if exists pilot_trips;
+DROP VIEW if exists trip_attractions_list;
+
+DROP TABLE if exists countries CASCADE;
+DROP TABLE if exists cities CASCADE;
+DROP TABLE if exists travels CASCADE;
+DROP TABLE if exists travels_discounts CASCADE;
+DROP TABLE if exists attractions CASCADE;
+DROP TABLE if exists trips CASCADE;
+DROP TABLE if exists pilots CASCADE;
+DROP TABLE if exists trip_routes CASCADE;
+DROP TABLE if exists clients CASCADE;
+DROP TABLE if exists client_statuses CASCADE;
+DROP TABLE if exists statuses CASCADE;
+DROP TABLE if exists client_trips CASCADE;
+DROP TABLE if exists attraction_discounts CASCADE;
+DROP TABLE if exists discounts CASCADE;
+DROP TABLE if exists trip_dates CASCADE;
+DROP TABLE if exists pilot_warrants CASCADE;
+DROP TABLE if exists travel_price_changes CASCADE;
+DROP TABLE if exists trip_attractions CASCADE;
+
+DROP FUNCTION if exists country_id(char);
+DROP FUNCTION if exists city_id(char);
+DROP FUNCTION if exists find_pilot;
+DROP FUNCTION if exists travels_both_directions;
+DROP FUNCTION if exists your_a_discount;
+DROP FUNCTION if exists your_t_discount;
+DROP FUNCTION if exists real_price;
+
+DROP SEQUENCE if exists foo;
+DROP SEQUENCE if exists seq;
+
+DROP FUNCTION if exists check1;
+DROP FUNCTION if exists check2;
+DROP FUNCTION if exists check3;
+DROP FUNCTION if exists check4;
+DROP FUNCTION if exists check5;
+DROP FUNCTION if exists check6;
+DROP FUNCTION if exists check7;
+DROP FUNCTION if exists check8;
+DROP FUNCTION if exists check9;
+DROP FUNCTION if exists check10;
+DROP FUNCTION if exists check11;
+DROP FUNCTION if exists check12;
+DROP FUNCTION if exists check13;
+DROP FUNCTION if exists check14;
+DROP FUNCTION if exists check15;
+DROP FUNCTION if exists check16;
+DROP FUNCTION if exists check17;
+DROP FUNCTION if exists check18;
+DROP FUNCTION if exists check19;
+DROP FUNCTION if exists check20;
+DROP FUNCTION if exists check21;
+DROP FUNCTION if exists check22;
+DROP FUNCTION if exists check23;
+DROP FUNCTION if exists check24;
+DROP FUNCTION if exists check25;
+DROP FUNCTION if exists check26;
+DROP FUNCTION if exists check27;
+DROP FUNCTION if exists check28;
+DROP FUNCTION if exists pesel_check;
+DROP FUNCTION if exists date_check;
+DROP FUNCTION if exists date_check2;
+DROP FUNCTION if exists pilot_check;
+DROP FUNCTION if exists discount_check1;
+DROP FUNCTION if exists discount_check2;
+
+DROP TYPE if exists attraction_type;
+DROP TYPE if exists travel_type;
+commit;

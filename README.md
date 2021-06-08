585a468f1edcecd95e2e3bcec90719278f4ea778
Paweł Nowak <pavlnowak@gmail.com>
2021-06-08 17:20:01 +0200

    Compiled

---
+++ README.md
@@ -0,0 +1,3 @@
+Run with
+java --module-path /usr/local/javafx-sdk-16/lib/ --add-modules javafx.controls,javafx.fxml -jar out/artifacts/ID_Project_jar/ID-Project.jar
+
---
+++ out/production/ID-Project/resources/other/base.sql
@@ -0,0 +1,22 @@
+INSERT INTO clients VALUES(10001,'dd', 'dd', '56122325866', '65');
+INSERT INTO clients VALUES(10002,'hghg', 'ghgh', '56122325866', '555');
+INSERT INTO clients VALUES(10003,'dddd', 'ddddd', '56122325866', '33333');
+INSERT INTO clients VALUES(10004,'gggggggg', 'ggggg', '56122325866', '54');
+INSERT INTO clients VALUES(10005,'sds', 'sds', '86061299887', '667');
+INSERT INTO clients VALUES(10006,'dfdf', 'dfdfd', '86061299887', '3333');
+INSERT INTO clients VALUES(10007,'hhhhhh', 'hhhh', '86061299887', '99');
+INSERT INTO clients VALUES(10008,'jh', 'jh', '86061299887', '66');
+INSERT INTO clients VALUES(10009,'hgg', 'hhhh', '86061299887', '65');
+INSERT INTO clients VALUES(10010,'xx', 'xx', '72052047764', '32');
+Update client_trips set paid_amount=980.812 where id=2 ;
+Delete from client_trips where id=2 ;
+Insert into opinions values(2,'Super!
+asqasd');
+Insert into opinions(opinion) values('Mega!');
+Insert into opinions(client_id,opinion) values(2,'OK');
+Delete from opinions where client_id=2 and opinion='Super!
+asqasd' ;
+INSERT INTO client_trips(trip_id,client_id,paid_amount) VALUES(1,2, 0);
+INSERT INTO client_trips(trip_id,client_id,paid_amount) VALUES(203,2, 0);
+INSERT INTO client_trips(trip_id,client_id,paid_amount) VALUES(145,1, 0);
+Insert into opinions(client_id,opinion) values(2,'Fantastic');
